593d67e157308aeeec1890df1af5bc19ce4e8d1d
Michael Rueda <michael.a.rueda22@gmail.com>
2024-06-21 12:40:34 -0400

    Render bars and group elements based on scaled data

--- example.js
+++ example.js
@@ -15,3 +15,16 @@ const xScale = d3.scaleliner()
         .domain([0, d3.max(dataset)])
         .range([50, svgWidth]);
 
+const barGroups = svg.selectAll("g")
+        .data(dataset)
+        .enter()
+        .append("g")
+        .attr("transform", (d, i) => 'translate(0, ${i * (barHeight + barMargin)})');
+
+barGroups.append("rect")
+        .attr("class", "bar")
+        .attr("width", 0)
+        .attr("height", barHeight)
+        .transition()
+        .duration(800)
+        .attr("width", d => xScale(d));
